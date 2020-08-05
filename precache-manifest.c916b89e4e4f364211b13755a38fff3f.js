4951a3e66b8c893c63da2c29bf23f1f1aae10120
Orlando Garcia <ogarcia@estrasol.com.mx>
2020-08-04 19:34:05 -0500

    Updates

---
+++ precache-manifest.c916b89e4e4f364211b13755a38fff3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ab0c48ef2930481125da9b961c2508",
+    "url": "/rick-morty-test/index.html"
+  },
+  {
+    "revision": "1ad3b4016290fc7878f8",
+    "url": "/rick-morty-test/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "58fb2d94ad5430bbbafb",
+    "url": "/rick-morty-test/static/js/2.d4660ba4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-morty-test/static/js/2.d4660ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad3b4016290fc7878f8",
+    "url": "/rick-morty-test/static/js/main.6db49bc4.chunk.js"
+  },
+  {
+    "revision": "bd9f261d190930ccd506",
+    "url": "/rick-morty-test/static/js/runtime-main.66a70e24.js"
+  }
+]);
